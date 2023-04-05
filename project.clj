51e7f16a6b7bc0e19030c2f14e29f881a94e2ef9
Thomas Nelson <tanelso2@gmail.com>
2023-04-05 15:07:47 -0500

    Figuring out how to set up the release process
    Run `lein release` to bump release and then bump to next patch version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tanelso2/clj-toolbox "0.2.2"
+(defproject org.clojars.tanelso2/clj-toolbox "0.3.6-SNAPSHOT"
   :description "A collection of tools for use in other Clojure projects"
   :url "https://github.com/tanelso2/clj-toolbox"
   :license {:name "Eclipse Public License"
@@ -10,18 +10,24 @@
             [lein-ancient "0.6.15"]
             [lein-changelog "0.3.2"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}}
-  :deploy-repositories [["releases" {:url :clojars :sign-releases false}]
-                        ["clojars" {
-                                    :sign-releases false}]]
+  :deploy-repositories [["releases" {:url "https://repo.clojars.org"
+                                     :sign-releases false
+                                     :username :env/clojars_username
+                                     :password :env/clojars_deploy_key}]
+                        ["snapshots" :clojars]
+                        ["clojars" {:sign-releases false
+                                    :username :env/clojars_username
+                                    :password :env/clojars_deploy_key}]]
   :source-paths ["src/clj" "src/cljc" "src/cljs"]
   :test-paths ["test/clj" "test/cljc" "test/cljs"]
   :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[clj-toolbox \"[0-9.]*\"\\\\]/[clj-toolbox \"${:version}\"]/" "README.md"]}
   :release-tasks [["shell" "git" "diff" "--exit-code"]
-                  ["change" "version" "leiningen.release/bump-version"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["changelog" "release"]
                   ["update-readme-version"]
                   ["vcs" "commit"]
-                  ["vcs" "tag"]
+                  ["vcs" "tag" "--no-sign"]
                   ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version" "patch"]
+                  ["vcs" "commit"]
                   ["vcs" "push"]])
