27a9c814e9c6dfdeaab36650fac846eaf43763fb
Thomas Nelson <tanelso2@gmail.com>
2023-04-05 15:07:47 -0500

    Don't sign git tags

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tanelso2/clj-toolbox "0.2.2"
+(defproject org.clojars.tanelso2/clj-toolbox "0.2.6-SNAPSHOT"
   :description "A collection of tools for use in other Clojure projects"
   :url "https://github.com/tanelso2/clj-toolbox"
   :license {:name "Eclipse Public License"
@@ -10,9 +10,8 @@
             [lein-ancient "0.6.15"]
             [lein-changelog "0.3.2"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}}
-  :deploy-repositories [["releases" {:url :clojars :sign-releases false}]
-                        ["clojars" {
-                                    :sign-releases false}]]
+  :deploy-repositories [["releases" {:url "https://repo.clojars.org" :creds :gpg}];}]
+                        ["clojars" {:sign-releases false}]]
   :source-paths ["src/clj" "src/cljc" "src/cljs"]
   :test-paths ["test/clj" "test/cljc" "test/cljs"]
   :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[clj-toolbox \"[0-9.]*\"\\\\]/[clj-toolbox \"${:version}\"]/" "README.md"]}
@@ -22,6 +21,6 @@
                   ["changelog" "release"]
                   ["update-readme-version"]
                   ["vcs" "commit"]
-                  ["vcs" "tag"]
+                  ["vcs" "tag" "--no-sign"]
                   ["deploy"]
                   ["vcs" "push"]])
