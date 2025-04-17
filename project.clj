9a0a71fd935b62819274ee407ed25afe307d2016
Thomas Nelson <tanelso2@gmail.com>
2025-04-17 17:40:28 -0500

    Add aliases from generating documentation and activating flow-storm

--- project.clj
+++ project.clj
@@ -9,10 +9,17 @@
             [lein-shell "0.5.0"]
             [lein-ancient "0.6.15"]
             [lein-changelog "0.3.2"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.4"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.12.0"]
                                   [org.clojure/test.check "1.1.1"]
                                   [com.gfredericks/test.chuck "0.2.13"]
-                                  [clj-http "3.12.2"]]}}
+                                  [clj-http "3.12.2"]]}
+             :docs {:plugins [[lein-html5-docs "3.0.3"]]
+                    :html5-docs-ns-includes #"^clj-toolbox\..*"
+                    :html5-docs-repository-url "https://github.com/tanelso2/clj-toolbox/blob/main"
+                    :html5-docs-docs-dir "target/docs"}
+             :flow-storm {:dependencies [[com.github.flow-storm/clojure "1.12.0-9"]
+                                         [com.github.flow-storm/flow-storm-dbg "4.2.1"]]
+                          :exclusions [org.clojure/clojure]}}
   :deploy-repositories [["releases" {:url "https://repo.clojars.org"
                                      :sign-releases false
                                      :username :env/clojars_username
@@ -24,6 +31,8 @@
   :source-paths ["src/clj" "src/cljc" "src/cljs"]
   :test-paths ["test/clj" "test/cljc" "test/cljs"]
   :aliases {"bump-version" ["change" "version" "leiningen.release/bump-version"]
+            "flow" ["with-profile" "+flow-storm" "repl"]
+            "gen-docs" ["with-profile" "+docs" "html5-docs"]
             "update-readme-version" ["shell" "sed" "-i" "s/\\\\[clj-toolbox \"[0-9.]*\"\\\\]/[clj-toolbox \"${:version}\"]/" "README.md"]}
   :release-tasks [["vcs" "assert-committed"]
                   ; TODO:
