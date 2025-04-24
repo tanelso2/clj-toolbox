457847f075198ace20c6f8e960934a34b3a14e0f
Thomas Nelson <tanelso2@gmail.com>
2025-04-23 23:43:01 -0500

    Version 1.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tanelso2/clj-toolbox "1.0.1"
+(defproject org.clojars.tanelso2/clj-toolbox "1.0.2-SNAPSHOT"
   :description "A collection of tools for use in other Clojure projects"
   :url "https://github.com/tanelso2/clj-toolbox"
   :license {:name "Eclipse Public License"
